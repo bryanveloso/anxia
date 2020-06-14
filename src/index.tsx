049ebace9bb4765ee53c81b4ec053492cf3fc634
Bryan Veloso <bryan@avalonstar.com>
2020-06-14 13:04:21 -0700

    chore: Initialize styled-system.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import { registerRootComponent } from 'expo'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 
-import store from './store'
 import Navigation from './navigation'
+import store from './store'
+import theme from './styles/theme'
 
 function App() {
   return (
     <Provider store={store}>
-      <Navigation />
+      <ThemeProvider theme={theme}>
+        <Navigation />
+      </ThemeProvider>
     </Provider>
   )
 }
 
-registerRootComponent(App);
+registerRootComponent(App)
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  colors: {
+    black: '#000e1a',
+    white: '#fff',
+    blue: '#007ce0',
+    navy: '#004175',
+    twitterBlue: '#1da1f2',
+  },
+}
