22434898af87a3b555d159f50939c40d721ff480
Bryan Veloso <bryan@avalonstar.com>
2020-06-14 12:42:05 -0700

    feat: Add string constants.

--- src/components/atoms/login-button.tsx
+++ src/components/atoms/login-button.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { ActivityIndicator, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
 import { useSelector } from 'react-redux'
 
+import { Strings } from '../../constants'
 import { useAppDispatch, RootState } from '../../store'
-import { login, AccessTokenState } from '../../store/login'
+import { login, AccessTokenState } from '../../store/auth'
 
 export default function LoginButton(props: TouchableOpacityProps) {
-  const accessTokenState = useSelector((state: RootState) => state.login.accessToken)
+  const accessTokenState = useSelector((state: RootState) => state.auth.accessToken)
   const dispatch = useAppDispatch()
 
   return (
+    // @ts-ignore
     <TouchableOpacity {...props} onPress={() => dispatch(login())}>
       {accessTokenState === AccessTokenState.Fetching ?
         <ActivityIndicator /> : (
-          <><Text>Login with Twitter</Text></>
+          <><Text>{Strings.AUTHENTICATION_BUTTON_TEXT}</Text></>
         )
       }
     </TouchableOpacity>
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const Strings = {
+  WELCOME_MESSAGE: '',
+  WELCOME_INSTRUCTIONS: '',
+  AUTHENTICATION_BUTTON_TEXT: 'Log in',
+  DATA_DISCLAIMER: "We don't store your data.",
+  LOG_OUT_ALERT_MESSAGE: 'Are you sure you want to log-out?',
+  LOG_OUT_ALERT_TITLE: 'Log out',
+  LOG_OUT_ALERT_CANCEL: 'Cancel',
+}
