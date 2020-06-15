47d316992cb1eece690255e03fe43e800556a404
Bryan Veloso <bryan@avalonstar.com>
2020-06-14 17:24:57 -0700

    fix: Add enableScreens().

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { registerRootComponent } from 'expo'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { enableScreens } from 'react-native-screens'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 
 import Navigation from './navigation'
 import store from './store'
 import theme from './styles/theme'
+
+enableScreens()
 
 function App() {
   return (
