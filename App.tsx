a75fbf2708349440b0a4a02dfda703cf7bd766e9
Bryan Veloso <bryan@velo.so>
2022-04-05 12:54:37 -0700

    Okay this sorta works.

--- App.tsx
+++ App.tsx
@@ -1,106 +1,183 @@
-import React, { useCallback, useState } from 'react';
-import * as AuthSession from 'expo-auth-session';
-import {
-  ActivityIndicator,
-  Button,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button, Platform } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
-const requestTokenURL = 'http://localhost:3000/request-token';
-const accessTokenURL = 'http://localhost:3000/access-token';
+const useProxy = Platform.select({ web: false, default: true });
 
-const redirect = AuthSession.makeRedirectUri();
-// This is the callback or redirect URL you need to whitelist in your Twitter app
-console.log(`Callback URL: ${redirect}`);
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://twitter.com/i/oauth2/authorize',
+  tokenEndpoint: 'https://twitter.com/i/oauth2/token',
+  revocationEndpoint: 'https://twitter.com/i/oauth2/revoke',
+};
 
 export default function App() {
-  const [username, setUsername] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: 'RlZaYjdOVW1TYlhKRWsycXBmNEs6MTpjaQ',
+      clientSecret: 'z6EeEp6858TSq11BazGSYWVNBo7ONK-Rd6qaBBZtrI_FLFAB9P',
+      redirectUri: makeRedirectUri({
+        useProxy,
+      }),
+      usePKCE: true,
+      scopes: ['tweet.read'],
+    },
+    discovery
+  );
 
-  const onLogout = useCallback(() => {
-    setUsername();
-    setLoading(false);
-    setError();
-  }, []);
+  React.useEffect(() => {
+    console.log(response);
 
-  const onLogin = useCallback(async () => {
-    setLoading(true);
-
-    try {
-      // Step #1 - first we need to fetch a request token to start the browser-based authentication flow
-      const requestParams = toQueryString({ callback_url: redirect });
-      const requestTokens = await fetch(requestTokenURL + requestParams).then(
-        (res) => res.json()
-      );
-
-      console.log('Request tokens fetched!', requestTokens);
-
-      // Step #2 - after we received the request tokens, we can start the auth session flow using these tokens
-      const authResponse = await AuthSession.startAsync({
-        authUrl:
-          'https://api.twitter.com/oauth/authenticate' +
-          toQueryString(requestTokens),
-      });
-
-      console.log('Auth response received!', authResponse);
-
-      // Validate if the auth session response is successful
-      // Note, we still receive a `authResponse.type = 'success'`, thats why we need to check on the params itself
-      if (authResponse.params && authResponse.params.denied) {
-        return setError('AuthSession failed, user did not authorize the app');
-      }
-
-      // Step #3 - when the user (successfully) authorized the app, we will receive a verification code.
-      // With this code we can request an access token and finish the auth flow.
-      const accessParams = toQueryString({
-        oauth_token: requestTokens.oauth_token,
-        oauth_token_secret: requestTokens.oauth_token_secret,
-        oauth_verifier: authResponse.params.oauth_verifier,
-      });
-      const accessTokens = await fetch(accessTokenURL + accessParams).then(
-        (res) => res.json()
-      );
-
-      console.log('Access tokens fetched!', accessTokens);
-
-      // Now let's store the username in our state to render it.
-      // You might want to store the `oauth_token` and `oauth_token_secret` for future use.
-      setUsername(accessTokens.screen_name);
-    } catch (error) {
-      console.log('Something went wrong...', error);
-      setError(error.message);
-    } finally {
-      setLoading(false);
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      console.log(code);
     }
-  }, []);
+  }, [response]);
 
   return (
     <View style={styles.container}>
-      {username !== undefined ? (
-        <View>
-          <Text style={styles.title}>Hi {username}!</Text>
-          <Button title="Logout to try again" onPress={onLogout} />
-        </View>
-      ) : (
-        <View>
-          <Text style={styles.title}>Example: Twitter login</Text>
-          <Button title="Login with Twitter" onPress={onLogin} />
-        </View>
-      )}
-
-      {error !== undefined && <Text style={styles.error}>Error: {error}</Text>}
-
-      {loading && (
-        <View style={[StyleSheet.absoluteFill, styles.loading]}>
-          <ActivityIndicator color="#fff" size="large" animating />
-        </View>
-      )}
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync({ useProxy });
+        }}
+      />
     </View>
   );
 }
+
+// import React, { useCallback, useState } from 'react';
+// import * as AuthSession from 'expo-auth-session';
+
+// import { TwitterApi } from 'twitter-api-v2';
+
+// // const requestTokenURL = 'http://localhost:3000/request-token';
+// // const accessTokenURL = 'http://localhost:3000/access-token';
+
+// // const redirect = AuthSession.makeRedirectUri();
+// // // This is the callback or redirect URL you need to whitelist in your Twitter app
+// // console.log(`Callback URL: ${redirect}`);
+
+// const discovery = {
+//   authorizationEndpoint: 'https://twitter.com/i/oauth2/authorize',
+//   tokenEndpoint: 'https://twitter.com/i/oauth2/token',
+//   revocationEndpoint: 'https://twitter.com/i/oauth2/revoke',
+// };
+
+// export default function App() {
+//   const [username, setUsername] = useState();
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState();
+
+//   const [request, response, promptAsync] = AuthSession.useAuthRequest(
+//     {
+//       redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+//       usePKCE: true,
+//       scopes: ['tweet.read'],
+//     },
+//     discovery
+//   );
+
+//   console.log('request', request);
+//   console.log('response', response);
+
+//   React.useEffect(() => {
+//     if (response?.type === 'success') {
+//       const { code } = response.params;
+//     }
+//   }, [response]);
+
+//   const onLogout = useCallback(() => {
+//     setUsername();
+//     setLoading(false);
+//     setError();
+//   }, []);
+
+//   // const onLogin = useCallback(async () => {
+//   //   setLoading(true);
+
+//   //   try {
+//   //     // Step #1 - first we need to fetch a request token to start the browser-based authentication flow
+//   //     const requestParams = toQueryString({ callback_url: redirect });
+//   //     const requestTokens = await fetch(requestTokenURL + requestParams).then(
+//   //       (res) => res.json()
+//   //     );
+
+//   //     console.log('Request tokens fetched!', requestTokens);
+
+//   //     // Step #2 - after we received the request tokens, we can start the auth session flow using these tokens
+//   //     const authResponse = await AuthSession.startAsync({
+//   //       authUrl:
+//   //         'https://api.twitter.com/oauth/authenticate' +
+//   //         toQueryString(requestTokens),
+//   //     });
+
+//   //     console.log('Auth response received!', authResponse);
+
+//   //     // Validate if the auth session response is successful
+//   //     // Note, we still receive a `authResponse.type = 'success'`, thats why we need to check on the params itself
+//   //     if (authResponse.params && authResponse.params.denied) {
+//   //       return setError('AuthSession failed, user did not authorize the app');
+//   //     }
+
+//   //     // Step #3 - when the user (successfully) authorized the app, we will receive a verification code.
+//   //     // With this code we can request an access token and finish the auth flow.
+//   //     const accessParams = toQueryString({
+//   //       oauth_token: requestTokens.oauth_token,
+//   //       oauth_token_secret: requestTokens.oauth_token_secret,
+//   //       oauth_verifier: authResponse.params.oauth_verifier,
+//   //     });
+//   //     const accessTokens = await fetch(accessTokenURL + accessParams).then(
+//   //       (res) => res.json()
+//   //     );
+
+//   //     console.log('Access tokens fetched!', accessTokens);
+
+//   //     // Now let's store the username in our state to render it.
+//   //     // You might want to store the `oauth_token` and `oauth_token_secret` for future use.
+//   //     setUsername(accessTokens.screen_name);
+//   //   } catch (error) {
+//   //     console.log('Something went wrong...', error);
+//   //     setError(error.message);
+//   //   } finally {
+//   //     setLoading(false);
+//   //   }
+//   // }, []);
+
+//   return (
+//     <View style={styles.container}>
+//       {username !== undefined ? (
+//         <View>
+//           <Text style={styles.title}>Hi {username}!</Text>
+//           <Button title="Logout to try again" onPress={onLogout} />
+//         </View>
+//       ) : (
+//         <View>
+//           <Text style={styles.title}>Example: Twitter login</Text>
+//           <Button
+//             title="Login with Twitter"
+//             onPress={() => {
+//               promptAsync({ useProxy: true });
+//             }}
+//           />
+//         </View>
+//       )}
+
+//       {error !== undefined && <Text style={styles.error}>Error: {error}</Text>}
+
+//       {loading && (
+//         <View style={[StyleSheet.absoluteFill, styles.loading]}>
+//           <ActivityIndicator color="#fff" size="large" animating />
+//         </View>
+//       )}
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -126,17 +203,17 @@ const styles = StyleSheet.create({
   },
 });
 
-/**
- * Converts an object to a query string.
- */
-function toQueryString(params) {
-  return (
-    '?' +
-    Object.entries(params)
-      .map(
-        ([key, value]) =>
-          `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-      )
-      .join('&')
-  );
-}
+// /**
+//  * Converts an object to a query string.
+//  */
+// function toQueryString(params) {
+//   return (
+//     '?' +
+//     Object.entries(params)
+//       .map(
+//         ([key, value]) =>
+//           `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+//       )
+//       .join('&')
+//   );
+// }
