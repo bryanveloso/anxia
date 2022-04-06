1429ca22d5c5b701df648dd1c3cfe78af42ac5fb
Bryan Veloso <bryan@velo.so>
2022-04-06 10:53:04 -0700

    Add next-auth.

---
+++ packages/next/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,12 @@
+import NextAuth from 'next-auth'
+import TwitterProvider from 'next-auth/providers/twitter'
+
+export default NextAuth({
+  providers: [
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      version: '2.0',
+    }),
+  ],
+})
